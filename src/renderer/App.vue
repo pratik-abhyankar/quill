71f509450ca09d0a4ea57d9c8e1b10bf463db10b
Pratik Abhyankar <pratik_abhyankar@persistent.co.in>
2018-05-28 15:06:22 +0530

    Initial commit. Also added Vuetify.

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-fade-transition>
+        <router-view></router-view>
+      </v-fade-transition>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'quill'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/Authentication/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-card-text>
+              <v-form>
+                <v-text-field 
+                  prepend-icon="person"
+                  name="username"
+                  label="Username"
+                  type="text">
+                </v-text-field>
+                <v-text-field
+                  id="password"
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  :type="passwordInput.type"
+                  :append-icon="passwordInput.icon"
+                  :append-icon-cb="togglePasswordVisiblity">
+                </v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" block>Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      isPasswordVisible: false,
+      passwordInput: { type: 'password', icon: 'visibility_off' }
+    }),
+    methods: {
+      togglePasswordVisiblity () {
+        this.isPasswordVisible = !this.isPasswordVisible
+        this.passwordInput = (this.isPasswordVisible)
+          ? { type: 'text', icon: 'visibility' }
+          : { type: 'password', icon: 'visibility_off' }
+      }
+    }
+  }
+</script>
